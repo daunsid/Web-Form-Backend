fce4fd5125f99b86367c33fa6278b81cc187aa16
Daniel Adeboye <oluwadaunsid@gmail.com>
2022-06-16 11:45:07 +0100

    created a Form application

---
+++ main.py
@@ -0,0 +1,42 @@
+from unicodedata import name
+from fastapi import Depends, FastAPI, Request, Form, requests
+#import jinja2
+#from pydantic import BaseModel
+from typing import Any, List
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+templates = Jinja2Templates(directory="templates")
+
+
+
+@app.post("/login", response_class=HTMLResponse)
+async def login(*, username:str=Form(...), password:str=Form(...)):
+
+    
+    return username
+    
+
+
+@app.get("/", response_class=HTMLResponse)
+async def main(request:Request):
+    return templates.TemplateResponse("form.html", context={"request":request})
+
+
+@app.post("/submit", response_class=HTMLResponse)
+def submit_file(*, request:Request, relate:str=Form(...),
+                worries:str=Form(...),
+                depends:str=Form(...),
+                trust:str=Form(...),
+                express:str=Form(...)):
+
+    print(relate)
+
+    return templates.TemplateResponse("response.html", context={"request":request})
